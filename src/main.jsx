60ab474130c2109ed03773498eaa583ab7982441
amnaakhtar1213 <amnabinterasheed1911@gmail.com>
2026-06-22 16:39:48 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/movie-app/">
+    <BrowserRouter basename="/movie-app">
           <App />
     </BrowserRouter>
   
